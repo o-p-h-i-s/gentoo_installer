4a35bc6ca40390730efd4557af408fc559f85507
o-p-h-i-s <80509766+o-p-h-i-s@users.noreply.github.com>
2022-03-22 18:16:18 +0900

    Update my_installer.sh

--- my_installer.sh
+++ my_installer.sh
@@ -84,7 +84,7 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-mount -bind /run /mnt/gentoo/run
+mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
 echo "Entering the new environment"
